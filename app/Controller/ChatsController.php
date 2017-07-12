04fea5e30c9be52d9642180736e8861c93c56b47
Jo Antipala <antipala.fdc01@gmail.com>
2017-07-12 16:45:42 +0800

    going to add user reg and login

--- app/Controller/ChatsController.php
+++ app/Controller/ChatsController.php
@@ -7,4 +7,12 @@ class ChatsController extends AppController {
 	public function index () {
 		
 	}
+
+	public function saveChat () {
+		$this->autoRender = false;
+		date_default_timezone_set('Asia/Manila');
+
+		echo 'save';
+
+	}
 }
